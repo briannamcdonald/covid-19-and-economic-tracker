a516a71f5a03188d34f1ee5206d365e3d93cee31
Brianna McDonald <brmcdonald@mun.ca>
2021-03-31 16:25:11 -0230

    added logo

--- app-frontend/src/components/Logo.js
+++ app-frontend/src/components/Logo.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Flex, Text, Image } from '@chakra-ui/react';
 
 const Logo = () => {
   return (
-    <Box>
-      <Text fontSize="lg" fontWeight="bold">
-        Logo
+    <Flex alignItems="center" justifyContent="center">
+      <Image src="./Images/logo.png" boxSize="2rem" borderRadius="md"></Image>
+      <Text
+        fontSize="lg"
+        fontWeight="bold"
+        marginLeft="0.5rem"
+        display={{ base: 'none', md: 'flex' }}
+      >
+        COVID-19 & Economic Tracker
       </Text>
-    </Box>
+    </Flex>
   );
 };
 
